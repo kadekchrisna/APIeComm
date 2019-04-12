1727251d31b8feba8e35e591d1190a4b1d550501
K <kadekchresna@gmail.com>
2019-04-12 22:00:05 +0700

    fixing bug in cart func

--- app/Controllers/Http/CartController.js
+++ app/Controllers/Http/CartController.js
@@ -231,7 +231,8 @@ class CartController {
         const idCheckout = parseInt(params.id)
         
         // const idProduct = parseInt(params.product)
-        if (qty >= 0) {
+
+        if (qty > 0) {
             const checkout = await Checkout.find(idCheckout)
             const total = checkout.price  * qty
             const idCart = checkout.cart_id
@@ -254,7 +255,7 @@ class CartController {
                 total: checkoutPrice[0].total
             })
 
-        } else if(qty = 0) {
+        } else if(qty < 0) {
             const checkout = await Checkout.find(idCheckout)
             const idCart = checkout.cart_id
             checkout.product_qty = 1
